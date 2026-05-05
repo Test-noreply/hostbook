01c9da6f5d424ada7d12d791f33eb09d3ddf99d4
Chochax <chochax55@gmail.com>
2026-05-05 16:52:01 +0200

    Ajout page de déconnexion

---
+++ deconnexion.php
@@ -0,0 +1,14 @@
+<?php
+// Démarrage de la session
+session_start();
+
+// Destruction de toutes les variables de session
+$_SESSION = array();
+
+// Destruction de la session
+session_destroy();
+
+// Redirection vers la page de connexion
+header("Location: connexion.php");
+exit();
+?>
