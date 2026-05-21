deea69b084199f4b6e328f2507386ff8dde9d062
COURJAUD Edward <edward.courjaud@etudiant.univ-rennes.fr>
2026-05-21 17:11:28 +0200

    test api

---
+++ intranet/api_fournisseurs.php
@@ -0,0 +1,21 @@
+<?php
+// api_fournisseurs.php
+header('Content-Type: application/json; charset=utf-8');
+
+// Autoriser le site WordPress à interroger cette API (Optionnel mais recommandé)
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+
+$fichier_fournisseurs = 'data/fournisseurs.json';
+
+if (file_exists($fichier_fournisseurs)) {
+    // On récupère directement le contenu du fichier JSON
+    $contenu = file_get_contents($fichier_fournisseurs);
+
+    // On l'affiche brut pour WordPress
+    echo $contenu;
+} else {
+    http_response_code(404);
+    echo json_encode(["error" => "Fichier de données introuvable."]);
+}
+exit();
